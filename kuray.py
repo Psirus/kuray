61a64c0485ff171741053c79dcbbc8a26ed537b3
Christoph Pohl <sam-ragga@web.de>
2013-09-09 16:51:31 +0200

    Transfer signal generation code to signals.py

--- kuray.py
+++ kuray.py
@@ -10,6 +10,7 @@ import pyaudio
 import PySide.QtGui as QtGui
 import PySide.QtCore as QtCore
 import smoothing
+import signals
 import sys
 
 CHUNK = 1024
@@ -26,7 +27,8 @@ class Gui(QtGui.QMainWindow):
         self.amplitude = []
         self.phase = []
         self.frequencies = []
-        self.length_in_seconds = 3
+        self.length = 3
+        self.signal = signals.Signal(30, 20e3, self.length)
         self.create_menu()
         self.create_main_frame()
 
@@ -39,12 +41,11 @@ class Gui(QtGui.QMainWindow):
                                  input=True, output=True, 
                                  frames_per_buffer = CHUNK)
 
-        length = CHUNK * round(self.length_in_seconds * RATE // CHUNK)
-        sweep = generate_sweep(length)
+        sweep = self.signal.generate_sweep()
 
         stream.write(sweep)
         answer = []
-        for _ in range(length//CHUNK):
+        for _ in range(self.signal.get_length() // CHUNK):
             data = stream.read(CHUNK)
             answer = np.append(answer, np.fromstring(data, 'Int16'))
 
@@ -54,8 +55,8 @@ class Gui(QtGui.QMainWindow):
         self.amplitude = np.abs(transfer_function)
         self.phase = np.angle(transfer_function)
 
-        f_min = 30
-        f_max = 20e3
+        f_min = self.signal.f_min
+        f_max = self.signal.f_max
         number_of_points = 4048
         frequency_ratio = math.log(f_max / f_min) / number_of_points
         self.frequencies = [math.exp(i*frequency_ratio) * f_min
@@ -126,7 +127,16 @@ class Gui(QtGui.QMainWindow):
         self.canvas.draw()
 
     def change_signal_length(self, length):
-        self.length_in_seconds = length
+        """ Change length of excitation signal """
+        self.length = length
+
+    def change_signal_f_min(self, f_min):
+        """ Change minimum frequency of excitation signal """
+        self.signal.f_min = f_min
+
+    def change_signal_f_max(self, f_max):
+        """ Change maximum frequency of excitation signal """
+        self.signal.f_max = f_max
 
     def create_main_frame(self):
         """ Create main frame within the main window. """
@@ -136,12 +146,28 @@ class Gui(QtGui.QMainWindow):
         signal_length_box = QtGui.QDoubleSpinBox(self)
         signal_length_box.setSuffix(" s")
         signal_length_box.setSingleStep(0.1)
-        signal_length_box.setValue(self.length_in_seconds)
+        signal_length_box.setValue(self.length)
         signal_length_box.valueChanged.connect(self.change_signal_length)
         signal_length_label = QtGui.QLabel(self)
         signal_length_label.setText("Length in seconds")
+        signal_f_min_box = QtGui.QDoubleSpinBox(self)
+        signal_f_min_box.setSuffix(" Hz")
+        signal_f_min_box.setValue(self.signal.f_min)
+        signal_f_min_box.setRange(20.0, 20e3)
+        signal_f_min_box.valueChanged.connect(self.change_signal_f_min)
+        signal_f_min_label = QtGui.QLabel(self)
+        signal_f_min_label.setText("Lowest frequency")
+        signal_f_max_box = QtGui.QDoubleSpinBox(self)
+        signal_f_max_box.setSuffix(" Hz")
+        signal_f_max_box.setRange(20.0, 20e3)
+        signal_f_max_box.setValue(self.signal.f_max)
+        signal_f_max_box.valueChanged.connect(self.change_signal_f_max)
+        signal_f_max_label = QtGui.QLabel(self)
+        signal_f_max_label.setText("Highest frequency")
         signal_param_hbox = QtGui.QFormLayout()
         signal_param_hbox.addRow(signal_length_label, signal_length_box)
+        signal_param_hbox.addRow(signal_f_min_label, signal_f_min_box)
+        signal_param_hbox.addRow(signal_f_max_label, signal_f_max_box)
         signal_param_group.setLayout(signal_param_hbox)
 
         smooth_group = QtGui.QGroupBox("Representation")
@@ -193,23 +219,6 @@ class Gui(QtGui.QMainWindow):
         self.axes2.set_xticklabels(ticklabel_frequencies)
 
         self.canvas.draw()
-
-def generate_sweep(length):
-    """ Generate sweep with `length` number of samples. """
-    amp = 2**15 - 1
-    f_start = 30.0 
-    f_stop = 20000
-    phi = 0.0
-    d_phi = 2*np.pi*f_start/RATE
-
-    sweep = np.zeros(length)
-    for i in range(length):
-        sweep[i] = amp*np.sin(phi)
-        phi += d_phi
-        d_phi *= 2**(np.log2(f_stop - f_start) / length)
-
-    sweep = np.int16(sweep)
-    return sweep
 
 
 def main():
---
+++ signals.py
@@ -0,0 +1,52 @@
+""" Create excitation signals and store related data """
+import numpy as np
+
+RATE = 44100
+CHUNK = 1024
+
+class Signal(object):
+    """ Model for excitation signal """
+
+    def __init__(self, f_min=30.0, f_max=20e3, length=3):
+        self._f_min = f_min
+        self._f_max = f_max
+        self.length = CHUNK * int(round(length * RATE // CHUNK))
+        
+    @property
+    def f_min(self):
+        """ Lowest frequency """
+        return self._f_min
+
+    @f_min.setter
+    def f_min(self, f_min):
+        """ Set minimum frequency """
+        self._f_min = f_min
+
+    @property
+    def f_max(self):
+        """ Highest frequency """
+        return self._f_max
+
+    @f_max.setter
+    def f_max(self, f_max):
+        """ Set maximum frequency """
+        self._f_max = f_max
+
+    def get_length(self):
+        """ Return length of signal in samples """
+        return self.length
+
+    def generate_sweep(self):
+        """ Generate sweep with `length` number of samples. """
+        amp = 2**15 - 1
+        phi = 0.0
+        d_phi = 2*np.pi*self.f_min/RATE
+
+        sweep = np.zeros(self.length)
+        for i in range(self.length):
+            sweep[i] = amp*np.sin(phi)
+            phi += d_phi
+            d_phi *= 2**(np.log2(self.f_max - self.f_min) / self.length)
+
+        sweep = np.int16(sweep)
+        return sweep
